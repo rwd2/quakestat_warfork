b876b22322fe79c2aabe84bc19edf81e95537050
rwd2 <967059+rwd2@users.noreply.github.com>
2020-09-29 12:29:26 +0200

    secure tempfile

--- quakestat_warfork.sh
+++ quakestat_warfork.sh
@@ -24,20 +24,23 @@
 
 SCRIPT=$(readlink -f "$0")
 SCRIPT_DIRECTORY=$(readlink -f $(dirname $SCRIPT))
+trap 'rm -f "$TMPFILE"' EXIT
+TMPFILE=$(mktemp) || exit 1
 
 main() {
     clear
     echo -e "\e[0;0mconnecting to server..."
-    qstat  -P -R -ne -u -xml -utf8 -retry 1 -warforkm master1.forbidden.gg |  sed 's/\^[0-9]//g' > /tmp/qstat_warfork.xml
+    qstat  -P -R -ne -u -xml -utf8 -retry 1 -warforkm master1.forbidden.gg |  sed 's/\^[0-9]//g' > $TMPFILE
     clear
-    xsltproc "$SCRIPT_DIRECTORY/quakestat.xsl" /tmp/qstat_warfork.xml  | xargs -0  echo -e
-    rm /tmp/qstat_warfork.xml
+    xsltproc "$SCRIPT_DIRECTORY/quakestat.xsl" $TMPFILE  | xargs -0  echo -e
+    rm $TMPFILE
     echo -e  "\e[0;35m"
     read -t 1 -n 1000 discard # discard keys typed during load
 }
 main
-while read -n1 -r -p " -> press space to reload, or any other key to quit" && [[ "$REPLY" = ' ' ]]; do
-    main
+while read -n1 -r -p " -> press space to reload, or any other key to quit" && [[ "$REPLY" = ' ' ]]; 
+do
+ main
 done
 clear
 
